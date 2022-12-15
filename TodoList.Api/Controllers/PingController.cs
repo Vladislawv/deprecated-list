f9315b1b483fc6c64a343cd3a1d98cbf2e60fc51
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2022-12-15 12:23:49 +0200

    Created and implemented ExceptionHandlingMiddleware #3
    feat - Created and implemented ExceptionHandlingMiddleware;
    feat - Created Ping controller with Ping endpoint.

---
+++ TodoList.Api/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Api.Controllers;
+
+[ApiController]
+[Route("api/ping")]
+[Produces(MediaTypeNames.Application.Json)]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public Task<IActionResult> PingAsync()
+    {
+        return Task.FromResult<IActionResult>(Ok());
+    }
+}
--- TodoList.Api/Program.cs
+++ TodoList.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TodoList.Api.Middlewares;
 using TodoList.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
