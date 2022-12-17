d9b077a2c411fcf2ab0bb1e2f4e4cfa092ea4b5d
Vladislawv <vladusupov2810@gmail.com>
2022-12-17 10:17:43 +0200

    feat - Created UnArchiveController that implemented UnArchiveService; chore - Removed unnecessary code.

---
+++ TodoList.Api/Controllers/UnArchiveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Services.Areas.Items;
+using TodoList.Services.Areas.Items.Dto;
+using TodoList.Services.Areas.UnArchive;
+
+namespace TodoList.Api.Controllers;
+
+[ApiController]
+[Route("api/unarchive")]
+[Produces(MediaTypeNames.Application.Json)]
+public class UnArchiveController : ControllerBase
+{
+    private readonly IUnArchiveService _unArchiveService;
+    private readonly IItemService _itemService;
+
+    public UnArchiveController(IUnArchiveService unArchiveService, IItemService itemService)
+    {
+        _unArchiveService = unArchiveService;
+        _itemService = itemService;
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(typeof(ItemDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> UnArchiveByIdAsync([FromRoute] int id)
+    {
+        var itemId = await _unArchiveService.UnArchiveByIdAsync(id);
+        var item = await _itemService.GetByIdAsync(itemId);
+
+        return Ok(item);
+    }
+}
