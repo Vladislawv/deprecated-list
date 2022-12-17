9e0fef84df879545eddbd48615ac02b3b969a1bd
Vladislawv <87234450+Vladislawv@users.noreply.github.com>
2022-12-17 09:56:13 +0200

    Created ArchiveController #12
    feat - Created ArchiveController that implemented ArchiveService;
    fix - Fixed ItemService.

--- TodoList.Services/Areas/Archive/IArchiveService.cs
+++ TodoList.Services/Areas/Archive/IArchiveService.cs
@@ -1,12 +1,12 @@
-﻿using TodoList.Infrastructure.Entities;
-using TodoList.Services.Areas.Archive.Dto;
+﻿using TodoList.Services.Areas.Archive.Dto;
 
 namespace TodoList.Services.Areas.Archive;
 
 public interface IArchiveService
 {
     public Task<IList<ArchivedDto>> GetListAsync();
-    public Task ArchiveAsync(Item item);
+    public Task<ArchivedDto> GetByIdAsync(int id);
+    public Task<int> ArchiveByIdAsync(int id);
     public Task<int> UnArchiveByIdAsync(int id);
     public Task DeleteByIdAsync(int id);
 }
